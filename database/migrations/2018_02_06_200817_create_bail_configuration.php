59aced65ad000d7c9f71d7fe2c134a043a36cb48
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-02-07 15:17:11 -0500

    Added Configuration Seeder for bail_configuration

--- database/migrations/2018_02_06_200817_create_bail_configuration.php
+++ database/migrations/2018_02_06_200817_create_bail_configuration.php
@@ -18,6 +18,7 @@ class CreateBailConfiguration extends Migration
             $table->increments('bc_id');
             $table->dateTime('bc_create_at');
             $table->tinyInteger('bc_active');
+            $table->integer('bc_order');
             $table->string('bc_category', 20);
             $table->string('bc_type', 25);
             $table->string('bc_value', 50)->nullable();
