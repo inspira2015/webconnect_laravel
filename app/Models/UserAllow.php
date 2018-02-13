d7f559889567da3372253f98eb8d5b3fd45e82f5
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-02-13 10:37:06 -0500

    Added Model for UserAllow

---
+++ app/Models/UserAllow.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserAllow extends Model
+{
+	/**
+	 * [$table Db table name]
+	 * @var string
+	 */
+    protected $table = 'users_allow';
+
+    /**
+     * [$timestamps description]
+     * @var boolean
+     */
+    public $timestamps = false;
+
+    public function scopeGetUserClearance($query, $currentUserId)
+    {
+        $currentUserId = (int) $currentUserId;
+        return $query->where('ua_uid', '=', $currentUserId)->get();
+    }
+}
