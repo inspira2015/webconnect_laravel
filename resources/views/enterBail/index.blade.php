b6b3f6e420c3b3acf9a31540ef9879efb703a363
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-02-16 13:27:51 -0500

    Added link to EnterBail

--- resources/views/enterBail/index.blade.php
+++ resources/views/enterBail/index.blade.php
@@ -14,7 +14,7 @@
                 <td><span class="content" style="font-weight: bold;">Select a bail entry method </span><span class="content"></span></td>
             </tr>
             <tr>
-                <td height="21" style="background-image:url({{ asset('images/cashbail/cashbailbuttons_03.jpg') }}); background-position: left top; background-repeat:no-repeat"><span style="font-weight: bold; padding-left:30px;"><a href="jailimport.php" class="content">Enter Bail From Jail Check</a></span></td>
+                <td height="21" style="background-image:url({{ asset('images/cashbail/cashbailbuttons_03.jpg') }}); background-position: left top; background-repeat:no-repeat"><span style="font-weight: bold; padding-left:30px;"><a href="{{ route('jailcheck') }}" class="content">Enter Bail From Jail Check</a></span></td>
             </tr>
             <tr>
                 <td height="21" style="background-image:url({{ asset('images/cashbail/cashbailbuttons_03.jpg') }}); background-position: left top; background-repeat:no-repeat"><span style="font-weight: bold; padding-left:30px;"><a href="enterbail.php" class="content">Enter Bail Manually </a></span><a href="enterbail.php"><span class="content"></span></a></td>
