ab967148f9c94b244e49c32b9d6388664b7ddfb7
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-03-07 11:51:05 -0500

    Reapply - Remove unused variables

--- app/Http/Controllers/EnterBailController.php
+++ app/Http/Controllers/EnterBailController.php
@@ -56,11 +56,6 @@ class EnterBailController extends Controller
         $indexArray = [
                         'jailRecords' => array(),
                         'totalCheckAmount' => '',
-                        'color1' => "#EEEEEE",
-                        'color2' => "#CCCCCC",
-                        'row_count' => 0,
-                        'check_total' => 0,
-                        'keyno' => 0,
                         'courtList' => $courtList,
                         'processBail' => true,
                       ];
@@ -85,11 +80,6 @@ class EnterBailController extends Controller
                         'checkNumber' => $checkNumber,
                         'totalCheckAmount' => $totalJailRecords,
                         'jailRecords' => $jailRecords,
-                        'color1' => "#EEEEEE",
-                        'color2' => "#CCCCCC",
-                        'row_count' => 0,
-                        'check_total' => 0,
-                        'keyno' => 0,
                         'courtList' => $courtList,
                         'processBail' => true,
                       ];
@@ -175,11 +165,6 @@ class EnterBailController extends Controller
                             'checkNumber' => $checkNumber,
                             'totalCheckAmount' => $totalJailRecords,
                             'jailRecords' => $jailRecords,
-                            'color1' => "#EEEEEE",
-                            'color2' => "#CCCCCC",
-                            'row_count' => 0,
-                            'check_total' => 0,
-                            'keyno' => 0,
                             'courtList' => $courtList,
                             'processBail' => false,
                           ];
