8f1ab47850f71521f9b6df48fb39c6cfbd9c84a5
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-03-19 10:32:23 -0400

    Added new Menu Bar for Cash Bails

--- resources/views/enterBail/jailImport.blade.php
+++ resources/views/enterBail/jailImport.blade.php
@@ -73,7 +73,6 @@
     <table width="100%" border="0" cellspacing="0" cellpadding="2">
         <tr>
             <td valign="top"><h1>Enter Bail By Jail Check Number</h1></td>
-            <td valign="top"><div align="right"><span class="content"><a href="{{ route('home') }}" >Main Menu </a></span></div></td>
         </tr>
     </table>
 
