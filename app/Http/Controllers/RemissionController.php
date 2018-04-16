a40909459689dee550ad76cf416d76901c429556
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-04-16 14:42:39 -0400

    Added Remission search

--- app/Http/Controllers/RemissionController.php
+++ app/Http/Controllers/RemissionController.php
@@ -9,8 +9,13 @@ use App\Models\Courts;
 use App\Models\BailMaster;
 use App\Models\BailTransactions;
 use App\Models\BailConfiguration;
+use App\Models\BailComments;
 use App\Facades\CreateTransaction;
-
+use App\Facades\PostedData;
+use App\Facades\CountyFee;
+use App\Events\ValidateTransactionBalance;
+use Carbon\Carbon;
+use Event;
 use Redirect;
 use Auth;
 use Input;
@@ -43,8 +48,57 @@ class RemissionController extends Controller
                       ];
 
 
-        return view('enterBail.index')->with($indexArray);
+        return view('remission.index')->with($indexArray);
     }
 
+    public function searchresults33(Request $request)
+    {
+        $formData = $request->all();
+        dd($formData);
+        exit;
+
+    }
+
+    public function searchresults(Request $request)
+    {
+        $termToSearch   = $request->get('search_term','');
+        $resultArray    = PostedData::getTermFromUserInput($termToSearch);
+
+        if (is_numeric($resultArray['m_id']) == false) {
+            $messages = [
+                            "\"{$resultArray['m_id']}\" was not found",
+                        ];
+            return redirect()->route('processbailsearch')->withErrors($messages);
+        }
+        $bailMasterId      = (int) $resultArray['m_id'];
+        $bailMaster        = BailMaster::find($bailMasterId);
+        $courtList         = Courts::pluck('c_name', 'c_id')->toArray();
+        $stateList         = BailConfiguration::where('bc_category', 'states')->pluck('bc_value', 'bc_id')->toArray();
+        $courtCheckList    = BailConfiguration::where('bc_category', 'check_court')->pluck('bc_value', 'bc_id')->toArray();
+        $bailMaterComments = BailComments::GetBailMasterComments($bailMasterId);
+
+        $dt = new Carbon($bailMaster->m_posted_date);
+        $m_posted_date =  $dt->format("m/d/Y");
+
+        $resultBalance = Event::fire(new ValidateTransactionBalance($bailMaster));
+        $balance = round($resultBalance[0], 2);
+
+        $indexArray = [
+                        'jailRecords'    => array(),
+                        'bailMasterId'   => $bailMasterId,
+                        'balance'        => $balance,
+                        'stateList'      => $stateList,
+                        'courtList'      => $courtList,
+                        'courtCheckList' => $courtCheckList,
+                        'm_posted_date'  => $m_posted_date,
+                        'bailDetails'    => [
+                                             'total_balance'  => $balance,
+                                             'fee_percentaje' => CountyFee::getFeePercentaje(),
+                                             'fee_amount'     => CountyFee::getAmountFee($balance),
+                                             'remain_amount'  => CountyFee::getRemainAmountAfterFee($balance),
+                                            ],
+                      ];
+        return view('processbail.refundbails', compact('bailMaster', 'bailMaterComments'))->with($indexArray);
+    }
 
 }
---
+++ resources/views/remission/index.blade.php
@@ -0,0 +1,111 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div style="background-image:url(images/back_orange_21.jpg); background-position:top left; background-repeat:no-repeat;  padding:10px;">
+ <table width="100%" border="0" cellspacing="0" cellpadding="2">
+  <tr>
+   <td valign="top"><h1>Remissions</h1></td>
+  </tr>
+ </table>
+</div>
+
+<div class="container-800 center-screen">
+ <form name="remission-search" id="remission-search" method="post" action="{{ route('remissionsearch') }}" >
+  {{ csrf_field() }}
+  <div class="row">
+   <div class="col-8">
+    <div class="input-group">
+     <div class="input-group-btn search-panel">
+      <button type="button" class="btn-lg dropdown-toggle" data-toggle="dropdown">
+       <span id="search_concept">Filter by</span> <span class="caret"></span>
+      </button>
+      <ul class="dropdown-menu" role="menu">
+        <li><a href="#Index_Number">Index Number</a></li>
+        <li><a href="#Defendant_name">Defendant Name</a></li>
+        <li><a href="#Surety_name">Surety Name</a></li>
+        <li class="divider"></li>
+      </ul>
+     </div>
+     <input type="hidden" name="search_param" value="all" id="search_param">
+     <input type="hidden" name="autocomplete" value="" id="autocomplete">
+     <input type="text" name="search_term" id="search_term" class="form-control"  placeholder="Search term...">
+
+
+     <span class="input-group-btn">
+      <button class="btn btn-lg" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+     </span>
+    </div>
+    <p><label id="search_term-error" class="error" for="search_term"></label></p>
+   </div>
+  </div>
+   @if ( $errors->count() > 0 )
+    <ul>
+     @foreach( $errors->all() as $message )
+      <li class="error"><strong>{{ $message }}</strong></li>
+     @endforeach
+    </ul>
+   @endif
+ </form>
+</div>
+
+<script type="text/javascript">
+
+ $(document).ready(function(e){
+  $('.search-panel .dropdown-menu').find('a').click(function(e) {
+   e.preventDefault();
+   var param = $(this).attr("href").replace("#","");
+   $('#search_param').attr('value', param);
+   var concept = $(this).text();
+   $('.search-panel span#search_concept').text(concept);
+  });
+
+  $('.ui-autocomplete-input').click( function() {
+   console.log('yes');
+  });
+
+
+  src = "{{ route('searchBailMaster') }}";
+   $("#search_term").autocomplete({
+    change: function (event, ui) {
+     if (!ui.item) {
+      //http://api.jqueryui.com/autocomplete/#event-change -
+      // The item selected from the menu, if any. Otherwise the property is null
+      //so clear the item for force selection
+      $("#search_term").val("");
+     }
+   },
+   source: function(request, response) {
+    var search_param = $('#search_param').attr('value');
+    $.ajax({
+     url: src,
+     dataType: "json",
+     data: {
+      term : request.term,
+      search_term : search_param
+     },
+     success: function(data) {
+      response(data);
+
+     }
+    });
+   },
+   minLength: 2,
+
+  });
+ });
+
+ $( "#remission-search" ).validate({
+  /*debug: true,*/
+  success: function(label,element) {
+   label.hide();
+  },
+
+  rules: {
+   search_term: {
+    required: true,
+   }
+  }
+ });
+</script>
+@endsection
